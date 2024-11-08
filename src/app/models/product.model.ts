086adedb5914e2e38347df0d9043910f993ea97a
Clementine Digout <clementine.digout@yahoo.fr>
2024-11-08 16:00:35 -0500

    change price

--- src/app/models/product.model.ts
+++ src/app/models/product.model.ts
@@ -1,6 +1,6 @@
 export class Product {
-    title: string = 'myPlant';
-    price: string = '$0,99';
-    desc: string = 'This is a plant description, hourra'
-    src: string = 'img/pilea.jpg';
+  title: string = "myPlant";
+  price: number = 0.99;
+  desc: string = "This is a plant description, hourra";
+  src: string = "img/pilea.jpg";
 }
